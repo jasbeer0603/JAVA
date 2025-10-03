3462afdbd7d43abefc64b3de266590c71732351e
jasbeer0603 <jasbir.singh.saini0603@gmail.com>
2025-10-03 21:36:18 +0530

    Java Repo

---
+++ Two.java
@@ -0,0 +1,10 @@
+public class Two {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 4;
+        int z = x * y;
+        System.out.println("Length of the rectangle is : "+ x);
+        System.out.println("Breadth of the rectangle is : "+ y);
+        System.out.println("Area of the rectangle is : "+ z);
+    }    
+}
